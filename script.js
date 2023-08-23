53a18389423e60aa02e9d08559c5a4e353a1ba19
qwerty <qwerty@gmail.com>
2023-08-23 09:34:44 +0300

    lol

---
+++ script.js
@@ -0,0 +1,8 @@
+var X = +prompt('Введите первое число')
+var Y = +prompt('Введите второе число')
+var Z = +prompt('Введите третье число')
+var str = (X + Y + Z)
+var haha = prompt('Откройте консоль')
+
+console.log('Привет!')
+console.log(str / 2);
